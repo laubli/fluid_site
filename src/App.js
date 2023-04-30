5d9a4cb53d5282e5fe3fd92e5730240d08824a66
E21A359N <laureline.blivet@etu.univ-nantes.fr>
2023-04-30 21:36:59 +0200

    Changement nom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
-import React, { Suspense, useState } from 'react';
+import React from 'react';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Nav from './components/Nav';
 import Shop from './components/Shop';
 import Accueil from './components/Accueil';
 import DeckGuerrier from './components/DeckGuerrier';
 import DeckDragon from './components/DeckDragon';
-import { Loader, Html } from "@react-three/drei";
-import { Canvas } from '@react-three/fiber';
 
 const App = () => {
   const router = createBrowserRouter([
@@ -43,6 +41,5 @@ const App = () => {
 
   return <RouterProvider router={router}/>;
 };
-
 
 export default App;
